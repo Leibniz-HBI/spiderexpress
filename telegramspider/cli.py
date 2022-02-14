6345e6c77e20f5e2c39cb605dd2bbcf158e80ef3
Philipp Kessling <p.kessling@leibniz-hbi.de>
2022-02-14 12:58:07 +0100

    [add] add command

--- telegramspider/cli.py
+++ telegramspider/cli.py
@@ -10,11 +10,26 @@ def cli(ctx):
     log.debug('Hello!')
 
 
+def complete_project_name(ctx, param, incomplete):
+    """
+    Searches/lists all project confs in the current context
+    """
+    return [_.name for _ in ctx.app.projects if _.name.startswidth(incomplete)]
+
 @cli.command()
-@click.argument('config', type=click.File())
+@click.argument(
+    'config',
+    type=click.STRING(),
+    shell_complete=complete_project_name
+)
 @click.pass_context
-def start(ctx, config: Path):
+def start(ctx):
     pass
+
+@click.pass_context
+def add(ctx):
+    pass
+
 
 if __name__ == 'main':
     cli(app = Spider())
