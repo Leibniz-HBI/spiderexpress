84de26cfe82417c7e7f67fd135eeb7f2f0c051d4
Philipp Kessling <p.kessling@leibniz-hbi.de>
2023-03-22 16:00:06 +0100

    feat: add orm classes and functions

---
+++ ponyexpress/model.py
@@ -0,0 +1,166 @@
+"""Definitions for the ponyexpress ORM-model.
+
+It keeps track of the crawled data, the configuration and the current state of the crawler.
+"""
+# pylint: disable=R0903
+
+from typing import Dict, Tuple
+
+import sqlalchemy as sql
+from sqlalchemy import orm
+
+mapper_registry = orm.registry()
+type_lookup = {"Integer": sql.Integer, "Text": sql.Text}
+
+
+class Base(orm.DeclarativeBase):
+    """Base class for all models."""
+
+    def __repr__(self):
+        props = [
+            f"{key}={value}"
+            for key, value in self.__dict__.items()
+            if not key.startswith("_")
+        ]
+        return f"<{self.__class__.__name__} {' '.join(props)} />"
+
+
+class AppMetaData(Base):
+    """Application metadata."""
+
+    __tablename__ = "application_state"
+
+    id: orm.Mapped[str] = orm.mapped_column(primary_key=True, index=True)
+    version: orm.Mapped[int] = orm.mapped_column()
+    iteration: orm.Mapped[int] = orm.mapped_column()
+
+
+class SeedList(Base):
+    """Table of seed nodes for each iteration."""
+
+    __tablename__ = "seed_list"
+
+    id: orm.Mapped[str] = orm.mapped_column(primary_key=True, index=True)
+    status: orm.Mapped[str] = orm.mapped_column()
+    iteration: orm.Mapped[int] = orm.mapped_column()
+    last_crawled_at: orm.Mapped[str] = orm.mapped_column()
+
+
+def create_raw_edge_table(
+    name: str, spec_variadic: Dict[str, str]
+) -> Tuple[sql.Table, type["RawEdge"]]:
+    """Create an edge table dynamically.
+
+    parameters:
+        name: name of the table
+        spec_variadic: dict of variadic columns
+
+    returns:
+        table: the table
+    """
+    spec_fixed = [
+        sql.Column("source", sql.Text, primary_key=True, index=True),
+        sql.Column("target", sql.Text, primary_key=True, index=True),
+    ]
+
+    table = sql.Table(
+        name,
+        Base.metadata,
+        *spec_fixed,
+        *[
+            sql.Column(key, type_lookup.get(value))
+            for key, value in spec_variadic.items()
+        ],
+    )
+
+    class RawEdge:
+        """Unaggregated, raw edge."""
+
+        def __repr__(self):
+            return f"""<RawEdge {
+                ' '.join([f'{key}={value}' for key, value in self.__dict__.items() if not key.startswith('_')])
+            } />"""
+
+    mapper_registry.map_imperatively(RawEdge, table)
+
+    return table, RawEdge
+
+
+def create_aggregated_edge_table(
+    name: str, spec_variadic: Dict[str, str]
+) -> Tuple[sql.Table, type["AggEdge"]]:
+    """Create an aggregated edge table dynamically.
+
+    parameters:
+        name: name of the table
+        spec_variadic: dict of variadic columns
+
+    returns:
+        table: the table
+    """
+    spec_fixed = [
+        sql.Column("source", sql.Text, primary_key=True, index=True),
+        sql.Column("target", sql.Text, primary_key=True, index=True),
+        sql.Column("weight", sql.Integer),
+    ]
+
+    table = sql.Table(
+        name,
+        Base.metadata,
+        *spec_fixed,
+        *[
+            sql.Column(key, type_lookup.get(value))
+            for key, value in spec_variadic.items()
+        ],
+    )
+
+    class AggEdge:
+        """Aggregated edge."""
+
+        def __repr__(self):
+            return f"""<AggEdge {
+                ' '.join([f'{key}={value}' for key, value in self.__dict__.items() if not key.startswith('_')])
+            } />"""
+
+    mapper_registry.map_imperatively(AggEdge, table)
+
+    return table, AggEdge
+
+
+def create_node_table(
+    name: str, spec_variadic: Dict[str, str]
+) -> Tuple[sql.Table, type["Node"]]:
+    """Create a node table dynamically.
+
+    parameters:
+        name: name of the table
+        spec_variadic: dict of variadic columns
+
+    returns:
+        table: the table
+    """
+    spec_fixed = [
+        sql.Column("id", sql.Text, primary_key=True, index=True),
+    ]
+
+    table = sql.Table(
+        name,
+        Base.metadata,
+        *spec_fixed,
+        *[
+            sql.Column(key, type_lookup.get(value))
+            for key, value in spec_variadic.items()
+        ],
+    )
+
+    class Node:
+        """Node."""
+
+        def __repr__(self):
+            return f"""<Node {
+                ' '.join([f'{key}={value}' for key, value in self.__dict__.items() if not key.startswith('_')])
+            } />"""
+
+    mapper_registry.map_imperatively(Node, table)
+
+    return table, Node
---
+++ tests/test_model.py
@@ -0,0 +1,143 @@
+"""Test suite for the model module."""
+import pytest
+import sqlalchemy as sql
+from sqlalchemy.orm import Session
+
+from ponyexpress.model import (
+    AppMetaData,
+    Base,
+    SeedList,
+    create_aggregated_edge_table,
+    create_node_table,
+    create_raw_edge_table,
+)
+
+# pylint: disable=W0621
+
+
+@pytest.fixture
+def connection():
+    """Creates an in-memory database."""
+
+    connection = sql.create_engine("sqlite:///:memory:", echo=False)
+    yield connection
+    connection.dispose(close=True)
+
+
+@pytest.fixture
+def session(connection):
+    """Creates an in-memory session."""
+
+    session = Session(connection)
+
+    yield session
+
+    session.close()
+
+
+@pytest.fixture
+def create_tables(connection):
+    """Creates the tables."""
+    yield lambda: Base.metadata.create_all(connection)
+    Base.metadata.drop_all(connection)
+
+
+def test_create_raw_edge_table_with_session(session, create_tables):
+    """Test the creation of a raw edge table."""
+    _, RawEdge = create_raw_edge_table("raw_edges", {"weight": "Integer"})
+
+    create_tables()
+
+    edge = RawEdge(source="a", target="b", weight=1)
+    session.add(edge)
+    session.commit()
+
+    assert session.query(RawEdge).count() == 1
+
+
+def test_create_raw_edge_table():
+    """Test the creation of a raw edge table."""
+    table, RawEdge = create_raw_edge_table("raw_edges_2", {"weight": "Integer"})
+
+    assert table.name == "raw_edges_2"
+    assert len(table.columns) == 3
+
+    assert hasattr(RawEdge, "source")
+    assert hasattr(RawEdge, "target")
+    assert hasattr(RawEdge, "weight")
+
+
+def test_create_aggregated_edge_table_with_session(session, create_tables):
+    """Test the creation of an aggregated edge table."""
+    _, Edge = create_aggregated_edge_table("agg_edges", {"view_count": "Integer"})
+
+    create_tables()
+
+    edge = Edge(source="a", target="b", weight=1, view_count=1)
+    session.add(edge)
+    session.commit()
+
+    assert session.query(Edge).count() == 1
+
+
+def test_create_aggregated_edge_table():
+    """Test the creation of an aggregated edge table."""
+    table, Edge = create_aggregated_edge_table("agg_edges_2", {"view_count": "Integer"})
+
+    assert table.name == "agg_edges_2"
+    assert len(table.columns) == 4
+
+    assert hasattr(Edge, "source")
+    assert hasattr(Edge, "target")
+    assert hasattr(Edge, "view_count")
+
+
+def test_create_node_table(session, create_tables):
+    """Test the creation of a node table."""
+
+    _, Node = create_node_table("nodes", {"subscriber_count": "Integer"})
+
+    create_tables()
+
+    node = Node(id="a", subscriber_count=1)
+    session.add(node)
+    session.commit()
+
+    assert session.query(Node).count() == 1
+
+
+def test_create_node_table_with_session():
+    """Test the creation of a node table."""
+    table, Node = create_node_table("nodes2", {"subscriber_count": "Integer"})
+
+    assert table.name == "nodes2"
+    assert len(table.columns) == 2
+
+    assert hasattr(Node, "id")
+    assert hasattr(Node, "subscriber_count")
+
+
+def test_app_state_table(session, create_tables):
+    """Test the creation of a node table."""
+
+    create_tables()
+
+    app_state = AppMetaData(id="a", version=1, iteration=1)
+    session.add(app_state)
+    session.commit()
+
+    assert session.query(AppMetaData).count() == 1
+
+
+def test_seed_list_table(session, create_tables):
+    """Test the creation of a node table."""
+
+    create_tables()
+
+    seed_list = SeedList(
+        id="a", status="done", iteration=1, last_crawled_at="2023-01-01T13:37:00"
+    )
+    session.add(seed_list)
+    session.commit()
+
+    assert session.query(SeedList).count() == 1
