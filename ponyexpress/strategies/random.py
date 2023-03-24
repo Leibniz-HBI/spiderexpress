39946153a606e47b451660f9a4f4aec182d12a8c
Philipp Kessling <p.kessling@leibniz-hbi.de>
2023-03-24 17:50:25 +0100

    feat: add random sampler

---
+++ ponyexpress/strategies/random.py
@@ -0,0 +1,29 @@
+"""Random sampling strategy."""
+
+from typing import Any, Dict, List
+
+import pandas as pd
+
+
+def random_strategy(
+    edges: pd.DataFrame,
+    nodes: pd.DataFrame,
+    known_nodes: List[str],
+    configuration: Dict[str, Any],
+):
+    """Random sampling strategy."""
+    # split the edges table into edges _inside_ and _outside_ of the known network
+    mask = edges.target.isin(known_nodes)
+    edges_inward = edges.loc[mask, :]
+    edges_outward = edges.loc[~mask, :]
+
+    # select 10 edges to follow
+    edges_sampled = edges_outward.sample(n=configuration["n"], replace=False)
+
+    new_seeds = edges_sampled.target  # select target node names as seeds for the
+    # next layer
+    edges_to_add = pd.concat([edges_inward, edges_sampled])  # add edges inside the
+    # known network as well as the sampled edges to the known network
+    new_nodes = nodes.loc[nodes.name.isin(new_seeds), :]
+
+    return new_seeds, edges_to_add, new_nodes
