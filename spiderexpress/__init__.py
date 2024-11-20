b91893e0776bf6374dacec958b561b788b5ce3b7
Philipp Kessling <p.kessling@leibniz-hbi.de>
2024-11-20 17:08:31 +0100

    Add PlugIn to top-level module exports.

--- spiderexpress/__init__.py
+++ spiderexpress/__init__.py
@@ -1,6 +1,8 @@
 """.. include:: ../README.md"""
 
 from .spider import Spider
-from .types import Configuration
+from .types import Configuration, PlugIn
+
+__all__ = ["Spider", "Configuration", "PlugIn"]
 
 __version__ = "0.1.0a0"
