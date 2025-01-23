bc96b9e5d59d9cfab4edd8cd6db4b5c95c47f95c
Felix Victor Münch <flxvctr@protonmail.com>
2025-01-23 15:00:27 +0100

    create test random walk

---
+++ tests/strategies/test_random_walk.py
@@ -0,0 +1,5 @@
+# test whether existing strategy can be imported
+import spiderexpress.strategies.random
+
+# test whether random walk strategy can be imported
+import spiderexpress.strategies.random_walk
