767a992c0ccfb7614d7bec871ac1c1c792f09750
Felix Victor Münch <flxvctr@protonmail.com>
2025-01-23 16:20:56 +0100

    add spider test and random_walk.py

--- tests/strategies/test_random_walk.py
+++ tests/strategies/test_random_walk.py
@@ -1,5 +1,18 @@
-# test whether existing strategy can be imported
-import spiderexpress.strategies.random
-
-# test whether random walk strategy can be imported
 import spiderexpress.strategies.random_walk
+
+from pathlib import Path
+from spiderexpress.spider import Spider
+
+
+def test_spider_with_random_walk():
+    """Should instantiate a spider."""
+    spider = Spider()
+
+    assert spider is not None
+    assert spider.is_idle()
+
+    spider.start(Path("tests/stubs/sevens_grader_random_walk_test.pe.yml"))
+
+    assert spider.is_stopped()
+    assert spider.configuration is not None
+    assert spider.iteration > 0
